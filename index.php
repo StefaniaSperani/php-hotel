8234a56f0fea0df071b9e831462d2449cb4277cf
StefaniaSperani <thedarkcry@hotmail.it>
2022-11-28 15:09:32 +0100

    table with hotels

--- index.php
+++ index.php
@@ -62,13 +62,53 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style.css">
     <title>PHP Hotel</title>
 </head>
 
 <body>
     <main>
+        <div class="container">
+            <h1 class="py-3 text-center text-capitalize">Our Hotels <i class="fa-solid fa-star fa-lg"></i></h1>
 
+            <table class="table table-dark table-striped text-capitalize">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Distance to center</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($hotels as $item) { ?>
+                    <tr>
+                        <th scope="row">
+                            <?php echo $item['name'] ?>
+                        </th>
+                        <td>
+                            <?php echo $item['description'] ?>
+                        </td>
+                        <td>
+                            <?php echo $item['parking'] ?>
+                        </td>
+                        <td>
+                            <?php echo $item['vote'] ?>
+                        </td>
+                        <td>
+                            <?php echo $item['distance_to_center'] ?>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </main>
 </body>
 
